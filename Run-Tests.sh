040e906878a8400322179f6d830401dcf4e306ba
Pierre-Olivier Latour <pol@mac.com>
2014-04-30 15:41:22 -0700

    Update

--- Run-Tests.sh
+++ Run-Tests.sh
@@ -1,15 +1,30 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 CONFIGURATION="Release"
-
 BUILD_DIR="/tmp/TEMP"
 PRODUCT="$BUILD_DIR/$CONFIGURATION/TEMP"
+TRACE_OUTPUT="/tmp/trace.txt"
 
-rm -rf "$BUILD_DIR"
-xcodebuild -configuration "$CONFIGURATION" build "SYMROOT=$BUILD_DIR" > /dev/null
+# rm -rf "$BUILD_DIR"
+# xcodebuild -configuration "$CONFIGURATION" build "SYMROOT=$BUILD_DIR" > /dev/null
+# 
+# $PRODUCT
 
-$PRODUCT
+sudo rm -f "$TRACE_OUTPUT"
+sudo DYLD_SHARED_REGION=avoid dtrace -s "test.d" -o "$TRACE_OUTPUT" -c "$PRODUCT" > /dev/null
 
-sudo dtrace -s "test.d" -c "$PRODUCT"
+echo "=== LEAKS ===="
+
+sort -b "$TRACE_OUTPUT" | while read LINE; do
+  if [ "$LINE" != "" ]; then
+    CLASS=`echo -n "$LINE" | awk '{ print $1 }'`
+    COUNT=`echo -n "$LINE" | awk '{ print $2 }'`
+    if [ "$COUNT" != "0" ]; then
+      printf "%40s\t%s\n" "$CLASS" "$COUNT"
+    fi
+  fi
+done
+
+echo "======"
 
 echo "Success!"
--- test.d
+++ test.d
@@ -3,7 +3,8 @@
 
 pid$target:libobjc.A.dylib:class_createInstance:entry
 {
-  ptr1 = *(long*)copyin(arg0, 8);
+  ptr0 = arg0;
+  ptr1 = *(long*)copyin(ptr0, 8);
   ptr2 = *(long*)copyin((ptr1 + 32) & ~3, 8);
   flags = *(int*)copyin(ptr2, 4);
   ptr3 = (flags & (1 << 31)) || (flags & (1 << 30)) ? *(long*)copyin(ptr2 + 8, 8) : ptr2;
@@ -13,17 +14,11 @@ pid$target:libobjc.A.dylib:class_createInstance:entry
   @allocations[class] = sum(1);
 }
 
-pid$target:libobjc.A.dylib:object_dispose:entry
-/arg0 != 0/
-{
-  printf("HIT\n");
-}
-
 /* Don't use objc_destructInstance() which is used to recycle objects */
 pid$target:libobjc.A.dylib:object_dispose:entry
 /arg0 != 0/
 {
-  ptr0 = *(long*)copyin(arg0, 8);
+  ptr0 = *(long*)copyin(arg0, 8);  /* TODO: Getting ISA from object this way will not work for tagged pointers but they likely don't get disposed of anyway */
   ptr1 = *(long*)copyin(ptr0, 8);
   ptr2 = *(long*)copyin((ptr1 + 32) & ~3, 8);
   ptr3 = (flags & (1 << 31)) || (flags & (1 << 30)) ? *(long*)copyin(ptr2 + 8, 8) : ptr2;
